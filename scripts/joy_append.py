160a6de5eba522ff93b0654bdb18e821906a2ade
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2022-09-26 20:09:23 +0900

    fix stamp

--- scripts/joy_append.py
+++ scripts/joy_append.py
@@ -9,15 +9,17 @@ class JoyAppend(object):
         self.joy_in = rospy.get_param("~joy_in", ["joy_in1","joy_in2"])
         self.msg_in = []
         self.sub = []
+        self.last_stamp = rospy.Time.now()
         for topic_name , i in zip(self.joy_in, range(len(self.joy_in))):
             self.sub.append(rospy.Subscriber(topic_name, Joy, self.callback, callback_args=i, queue_size=1))
             self.msg_in.append(None)
 
     def callback(self, data, index):
         self.msg_in[index] = data
-        if all(msg != None for msg in self.msg_in):
+        if all(msg != None for msg in self.msg_in) and self.last_stamp < data.header.stamp:
+            self.last_stamp = data.header.stamp
             msg_out = Joy()
-            msg_out.header.stamp = rospy.Time.now()
+            msg_out.header.stamp = data.header.stamp
             for msg in self.msg_in:
                 msg_out.axes += msg.axes
                 msg_out.buttons += msg.buttons
